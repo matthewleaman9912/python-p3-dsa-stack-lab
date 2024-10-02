3f4c72a52959936b31f591f9e579d3d32077d010
MatthewLeaman <leaman.30@osu.edu>
2024-10-01 23:28:55 -0400

    complete

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,32 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return self.size() == 0
 
     def push(self, item):
-        pass
+        self.items.append(item)
 
     def pop(self):
-        pass
-
+        if len(self.items) != 0:
+            return self.items.pop()
+        else:
+            return None
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            self.size() == 1
+            return True
+        else:
+            return None
 
     def search(self, target):
         pass
